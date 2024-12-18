057aa5768f2829581a1c7aa074b9977e05a22787
unknown <sadaniimalshasl@gmail.com>
2024-12-18 20:20:14 +0530

    with postman edits

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 
 
-function (){
-    console.log("hello");
-    
-}
 
+
+console.log(document.getElementById("chatBox"));
+
+
+
+function chatStart() {
+    let userInput = document.getElementById("txtUserInput").value;
+
+    document.getElementById("chatBox").innerHTML += 
+    
+    ` <li class="d-flex gap-4 justify-content-end">
+        <h5>${userInput}</h5>
+    </li> `;
+
+///////from the postman gemini
+
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "contents": [
+    {
+      "parts": [
+        {
+          "text": "Hello"
+        }
+      ]
+    }
+  ]
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBdXkfXoeGEf8BbUfTbbzPjOwpWJCNaHxE", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+
+}
